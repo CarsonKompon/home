0d7fbabb6fc1f2358974061b9de17d27f265c4bf
Carson Kompon <carsokompo@gmail.com>
2023-06-12 20:45:57 -0400

    anti-error technology

--- code/Entities/NPCs/ShopConstructionNPC.cs
+++ code/Entities/NPCs/ShopConstructionNPC.cs
@@ -13,7 +13,7 @@ public partial class ShopConstructionNPC : BaseNPC
 {
 
     public override string DisplayName => "#shop.construction";
-    protected override string ClothingString => "[{\"id\":626961084},{\"id\":-691668871},{\"id\":-620074038},{\"id\":-942608774},{\"id\":1045421669},{\"id\":1034075642},{\"id\":-477049300},{\"id\":-1441449628},{\"id\":-1492226718}]";
+    protected override string ClothingString => "[{\"id\":626961084},{\"id\":502735166},{\"id\":-620074038},{\"id\":-942608774},{\"id\":1045421669},{\"id\":1034075642},{\"id\":-477049300},{\"id\":-1441449628},{\"id\":-1492226718}]";
 
     public override void Spawn()
     {
